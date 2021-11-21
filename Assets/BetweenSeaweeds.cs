149efaff15e7f33a312791d85f394351dbdf8d03
deniurchak <swdenis@mail.ru>
2021-11-21 12:58:14 +0100

    change default bubble pop score to 5

--- Assets/BetweenSeaweeds.cs
+++ Assets/BetweenSeaweeds.cs
@@ -1,10 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BetweenSeaweeds : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerExit2D(Collider2D other)
     {
         if (OtherIsTheFish(other))
         {
