e8585a1558f8e224f612046059a50a2975ea5eb5
deniurchak <swdenis@mail.ru>
2021-11-22 18:34:44 +0100

    add text mesh pro and beautify assets

--- Assets/ScoreKeeper.cs
+++ Assets/ScoreKeeper.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ScoreKeeper : MonoBehaviour
 {
 
     private int _currentScore = 0;
 
-    Text _scoreText;
+    TMP_Text _scoreText;
 
     void Awake()
     {
-        _scoreText = GetComponent<Text>();
+        _scoreText = GetComponent<TMP_Text>();
     }
     public void incrementScore() {
         _currentScore++;
